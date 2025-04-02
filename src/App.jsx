fa6b088ad27d1b398785c99888afb259167aa25b
YMnooneJM <demraldo12@gmail.com>
2025-04-02 17:15:23 +0100

    NewToReact

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+// const App = () => {
+// // let firstName = "Mariam";
+// // let lastName = "Yusuf";
+// // let input = (<input tupe="text" placeholder="Name"/>);
+
+// // let button = <button>Submit</button>
+
+// // let form = (
+// //   <form>
+// //     {input}
+// //     {button}
+// //   </form>
+// // );
+// // return form;
+
+// // export default App;
+
+//   // return (
+//   // <div>
+//   //   <p>{firstName + " " + lastName}</p>
+//   // </div>
+//   // )
+// };
+
+const App = () => {
+  let pStyle = {
+    color: "red",
+    fontSize = 20,
+    textAlign:["center"]
+  }
+
+return (
+  <div>
+    <h1 style={{color:"black"}}>Welcome to Nigeria</h1>
+    <p style={pStyle}>This is our country, or not?</p>
+  </div>
+); 
+};
+
+export default App;
